abd3ba927b71f2ffc320c8c73228ac9674e56e08
sangheon <qwer55252@gmail.com>
2025-09-11 14:00:02 +0000

    Add: GigaSpeech OOD script 추가

---
+++ scripts/inference/OOD_inference_gigaspeech_fm_libri960.sh
@@ -0,0 +1,10 @@
+
+python asr_inference.py \
+  --ckpt_path "/workspace/KD-via-FM-in-ASR/outputs/FlowMatching_KD/layerwise_flowkd_mlp_linear_sampling8_libri960/checkpoints/last.ckpt" \
+  --use_ctc True \
+  --use_logit_distillation True \
+  --use_layerwise_distillation False \
+  --use_diffkd False \
+  --use_flow_matching True \
+  --data_dir /workspace/KD-via-FM-in-ASR/data/gigaspeech \
+  --eval_data gigaspeech
---
+++ scripts/inference/OOD_inference_gigaspeech_teacher_libri960.sh
@@ -0,0 +1,11 @@
+
+python asr_inference.py \
+  --ckpt_path "/workspace/KD-via-FM-in-ASR/outputs/FlowMatching_KD/libri960_teacher/checkpoints/last.ckpt" \
+  --use_ctc True \
+  --use_logit_distillation False \
+  --use_layerwise_distillation False \
+  --use_diffkd False \
+  --use_flow_matching False \
+  --data_dir /workspace/KD-via-FM-in-ASR/data/gigaspeech \
+  --eval_data gigaspeech 
+  
